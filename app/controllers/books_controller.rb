f9bb31bcccd05f75bfd7b22faf7f805e4c8f6f59
marlena-b <marlena.borowiec01@gmail.com>
2023-01-15 17:34:25 +0100

    Add new book form and create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,26 @@ class BooksController < ApplicationController
     @books = Book.page(params[:page])
   end
 
-  def show 
+  def show
     @book = Book.find(params[:id])
-  end 
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "Book created!"
+      redirect_to @book
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :publication_date, :publisher, :isbn, :isbn13, :description, :genre)
+  end
+
 end
