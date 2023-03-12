635dd5565cf84a0659cc6213a7955864d04d9f2c
marlena-b <marlena.borowiec01@gmail.com>
2023-03-12 16:02:09 +0100

    Add average rating

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,10 +2,15 @@
 
 class Book < ApplicationRecord
   has_one_attached :image
+  has_many :user_books, dependent: :destroy
 
   validates :title, length: { minimum: 3, maximum: 64 }, presence: true
   validates :author, length: { minimum: 3, maximum: 64 }, presence: true
   validates :genre, length: { minimum: 3, maximum: 64 }
   validates :isbn,   isbn_format: true, allow_blank: true
   validates :isbn13, isbn_format: { with: :isbn13 }, allow_blank: true
+
+  def average_rating
+    user_books.average(:rating)
+  end
 end
