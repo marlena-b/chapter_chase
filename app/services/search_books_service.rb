5e1e8a75b715f6eb2d2e12447e0a82f8b40d1d1b
marlena-b <marlena.borowiec01@gmail.com>
2023-07-06 00:02:30 +0200

    Add searching by genre and links in the top menu

--- app/services/search_books_service.rb
+++ app/services/search_books_service.rb
@@ -4,23 +4,42 @@ class SearchBooksService
   def initialize(params)
     @search_term = params[:search_term]
     @search_by = params[:search_by]
+    @genre = params[:genre]
   end
 
   # rubocop:disable Metrics/MethodLength
   def call
+    query = Book.all
+    query = apply_search(query)
+    query = by_genre(query)
+
+    query.order(:id).with_attached_image
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  private
+
+  def apply_search(query)
+    return query unless @search_by && @search_term
+
     case @search_by
     when 'title'
-      Book.where('title ILIKE :term', term: "%#{@search_term}%")
+      query.where('title ILIKE :term', term: "%#{@search_term}%")
     when 'author'
-      Book.where('author ILIKE :term', term: "%#{@search_term}%")
+      query.where('author ILIKE :term', term: "%#{@search_term}%")
     when 'isbn'
-      Book.where('isbn ILIKE :term', term: "%#{@search_term}%")
+      query.where('isbn ILIKE :term', term: "%#{@search_term}%")
     else
-      Book.where("title ILIKE :term
+      query.where("title ILIKE :term
         OR author ILIKE :term
         OR isbn ILIKE :term
         OR isbn13 ILIKE :term", term: "%#{@search_term}%")
-    end.order(:id).with_attached_image
+    end
   end
-  # rubocop:enable Metrics/MethodLength
+
+  def by_genre(query)
+    return query unless @genre
+
+    query.where(genre: @genre)
+  end
 end
